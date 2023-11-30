b468a5891eed75a8818eb78ecb670a2aebe19b5c
KonstantinMedvedev1996 <konstantin.medvedev.1996@gmail.com>
2023-11-30 11:03:12 +0500

    changeing in code

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import pandas as pd
 import seaborn as sns
 import json
 import joblib
+# import sklearn
 
 st.header('USA Real Estate Price Prediction')
 
 path_data = 'data/zip_mean_price.pickle'
-# path_unique_values = 'data/unique_values.json'
-# path_model = 'models/lr_pipeline.sav'
+path_unique_values = 'data/unique_values.json'
+path_model = 'models/model.sav'
 
 # df = pd.read_csv('data/russiarealestate.zip')
 
@@ -20,11 +21,11 @@ def load_data(path):
     # data = data.sample(5000)
     return data
 
-# @st.cache_data
-# def load_model(path):
-#     "Load model from path"
-#     model = joblib.load(path)
-#     return model
+@st.cache_data
+def load_model(path):
+    "Load model from path"
+    model = joblib.load(path)
+    return model
 
 @st.cache_data
 def transform(data):
@@ -41,50 +42,82 @@ def transform(data):
 
 df = load_data(path_data)
 df = transform(df)
-st.write(df[:10])
+# st.write(df[:10])
 
 st.map(data=df, latitude="latitude",longitude="longitude", color="label_colors")
 
-# with open(path_unique_values) as file:
-#     dict_unique = json.load(file)
+with open(path_unique_values) as file:
+    dict_unique = json.load(file)
 
 
 # #features
-# building_type = st.sidebar.selectbox('Building type',(dict_unique['building_type']))
-# object_type = st.sidebar.selectbox('Object type',(dict_unique['object_type']))
-# level = st.sidebar.slider ("Level", min_value=min(dict_unique["level"]), max_value=max(dict_unique["level"]))
-# levels = st.sidebar.slider ("Levels", min_value= min(dict_unique["levels"]), max_value = max(dict_unique["levels"]))
+status_type = st.sidebar.selectbox('Statusctype',(dict_unique['status']))
+object_type = st.sidebar.selectbox('Property type',(dict_unique['propertyType']))
+street_type = st.sidebar.selectbox('Street type',(dict_unique['street']))
+baths = st.sidebar.slider ("Bathrooms", min_value=min(dict_unique["baths"]), max_value=max(dict_unique["baths"]))
+fireplace = st.sidebar.selectbox('Fireplace',(dict_unique['fireplace']))
+# fireplace = st.sidebar.slider ("Fireplace", min_value= min(dict_unique["fireplace"]), max_value = max(dict_unique["fireplace"]))
+sqft = st.sidebar.slider ("Living space in sqft", min_value= min(dict_unique["sqft"]), max_value = max(dict_unique["sqft"]))
+beds = st.sidebar.slider ("Beds", min_value=min(dict_unique["beds"]), max_value=max(dict_unique["beds"]))
+# fireplace = st.sidebar.slider ("Fireplace", min_value= min(dict_unique["fireplace"]), max_value = max(dict_unique["fireplace"]))
+stories = st.sidebar.slider ("Stories", min_value=min(dict_unique["stories"]), max_value=max(dict_unique["stories"]))
+private_pool = st.sidebar.selectbox('Private Pool',(dict_unique['private_pool']))
+year_built = st.sidebar.slider ("Year built", min_value=min(dict_unique["year_built"]), max_value=max(dict_unique["year_built"]))
+heating_type = st.sidebar.selectbox('heating',(dict_unique['heating']))
+cooling = st.sidebar.selectbox('Cooling system',(dict_unique['cooling']))
+lot_size = st.sidebar.slider ("Lot size in sqft", min_value= min(dict_unique["lot_size"]), max_value = max(dict_unique["lot_size"]))
+school_rate_mean = st.sidebar.slider ("Schools' score", min_value= min(dict_unique["school_rate_mean"]), max_value = max(dict_unique["school_rate_mean"]))
+school_distance_mean = st.sidebar.slider ("Schools' distance", min_value= min(dict_unique["school_distance_mean"]), max_value = max(dict_unique["school_distance_mean"]))
+has_parking = st.sidebar.selectbox('Has parking?',(dict_unique['has_parking']))
+has_garage = st.sidebar.selectbox('Has garage?',(dict_unique['garage']))
+parking_spaces = st.sidebar.slider ("Parking spaces", min_value= min(dict_unique["parking_spaces"]), max_value = max(dict_unique["parking_spaces"]))
+states_listed = st.sidebar.selectbox('State',(dict_unique['states_shoted']))
+cities_listed = st.sidebar.selectbox('City',(dict_unique['cities_shorted']))
 # rooms = st.sidebar.selectbox('Rooms',(dict_unique['rooms']))
 # area = st.sidebar.slider ("Area", min_value=min(dict_unique["area"]), max_value = max(dict_unique["area"]))
 # kitchen_area = st.sidebar.slider("Kitchen area", min_value=min(dict_unique["kitchen_area"]), max_value = max(dict_unique["kitchen_area"])) 
 
-# dict_data = {
+dict_data = {
     
-#     "building_type": building_type,
-#     "object_type": object_type,
-#     "level": level,
-#     "levels": levels,
-#     "rooms" : rooms,
-#     "area": area,
-#     "kitchen_area":kitchen_area
-# }
+    "status": status_type,
+    "propertyType": object_type,
+    "street": street_type,
+    "baths": baths,
+    "fireplace" : fireplace,
+    "sqft": sqft,
+    "beds":beds,
+    "stories": stories,
+    "private_pool": private_pool,
+    "year_built": year_built,
+    "heating": heating_type,
+    "cooling": cooling,
+    "lot_size": lot_size,
+    "school_rate_mean": school_rate_mean,
+    "school_distance_mean": school_distance_mean,
+    "has_parking": has_parking,
+    "garage": has_garage,
+    "parking_spaces": parking_spaces,
+    "states_shoted": states_listed,
+    "cities_shorted": cities_listed
+       
+}
 
-# data_predict = pd.DataFrame([dict_data])
-# model = load_model(path_model)
+data_predict = pd.DataFrame([dict_data])
+model = load_model(path_model)
 
-# button_1 = st.button("Hello")
-# # button_2 = st.button("Predict_price")
-# button_3 = st.button("Predict")
+button_1 = st.button("Hello")
+# button_2 = st.button("Predict_price")
+button_3 = st.button("Predict")
 
-# if button_1:
-#     st.write('Why hello there')
+if button_1:
+    st.write('Why hello there')
     
-# # if button_2:
-# #     model.predict(data_predict)
+# if button_2:
+#     model.predict(data_predict)
 
-# if button_3:
-#     output = model.predict(data_predict)
-#     st.success(f"{round(output[0])} rub")
+if button_3:
+    output = model.predict(data_predict)
+    st.success(f"{round(output[0])} rub")
     
     
     
